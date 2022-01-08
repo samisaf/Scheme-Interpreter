4403ebf2b5fe10fc076671a2726626e5af64199e
Sami Safadi <samisaf@gmail.com>
2022-01-08 02:21:57 -0600

    changes

--- lisjs/main.ts
+++ lisjs/main.ts
@@ -1,11 +1,14 @@
-type Atom = string | number;
-type Expression = Atom | Array<Atom> | Array<Expression>
+type SchemeNumber = number; // Scheme number will be implemented as javascript number
+type SchemeString = string; // Scheme string will be implemented as javascript string
+type SchemeSymbol = string; // a symbol is a string
+type Atom = SchemeNumber | SchemeString | SchemeSymbol;
+type Expression = Atom | Array<Expression>;
 
 /**
  * Converts a string of characters into a list of tokens.
  */
  function tokenize(text: string): string[] {
-    return text.replaceAll("(", " ( ").replaceAll(")", " ) ").split(" ").filter(s => !isEmpty(s))
+    return text.replaceAll("(", " ( ").replaceAll(")", " ) ").split(" ").filter(s => !isEmpty(s));
   }
 
 function isEmpty(s: string){
@@ -17,29 +20,34 @@ function isEmpty(s: string){
  */
 function parseTokens(tokens: string[]): Expression {
   if (tokens.length === 0) throw SyntaxError("unexpected EOF");
-  const token = tokens.shift(); // pops first token
-  if (token === "(") {
-    const L = []
-    while (tokens[0] !== ")") L.push(parseTokens(tokens))
-    tokens.shift()
-    return L
+  else{
+    const token = tokens.shift(); // pops first token
+    if (token === "(") {
+      const L = [];
+      while (tokens[0] !== ")") L.push(parseTokens(tokens));
+      tokens.shift();
+      return L;
+    }
+    else if (token === ")") throw SyntaxError('unexpected )')
+    // @ts-ignore: token will always have a value as we checked for non-empty array
+    else return atom(token)
   }
-  else if (token === ")") throw SyntaxError('unexpected )')
-  else return atom(token || "")
 }
 
 /**
- * Numbers become numbers; every other token is a symbol.
+ * Numbers become numbers; otherwise, token is either a symbol or a string.
+ * Will have to differentiate between symbols and strings later
  */
 function atom(token: string): Atom{
   const parsed = Number(token)
   return isNaN(parsed)? String(token): parsed
-
 }
  
 let p1 = `
-(begin  (define r 10) 
-        (* pi (* r r))
+(begin  
+  (define name "Nice_Man")
+  (define r 10) 
+  (* pi (* r r))
 )`
 
 console.log(tokenize(p1))
